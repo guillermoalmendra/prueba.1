60e3d148e5b9d53cf418f625f382a40744113f72
guillermoalmendra <101884816+guillermoalmendra@users.noreply.github.com>
2023-05-19 11:27:04 -0400

    ASDDS

--- pagina/Venta.js
+++ pagina/Venta.js
@@ -1,92 +1,72 @@
 $(function()
-{
-    let numeros = '1234567890';
-    let letras  = 'qwertyuiopasdfghjklñzxcvbnmQWERTYUIOPASDFGHJKLÑZXCVBNM ';
-
-
-    $(".txtFolio").keydown(function(event){
-        //alert(event.keyCode);
-        if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105) && event.keyCode !==190  && event.keyCode !==110 && event.keyCode !==8 && event.keyCode !==9  ){
-            return false;
-        }
-    });
+    {
+        let numeros = '1234567890';
+        let letras  = 'qwertyuiopasdfghjklñzxcvbnmQWERTYUIOPASDFGHJKLÑZXCVBNM ';
+    
+        $('.txtApellido').keypress(function(e)
+        {
+            // obtener el caracter presionado por el usuario
+            let caracter = String.fromCharCode(e.which);
+            if(letras.indexOf(caracter) < 0)
+                return false;
+        })
+        $('.txtNombre').keypress(function(e)
+        {
+            // obtener el caracter presionado por el usuario
+            let caracter = String.fromCharCode(e.which);
+            if(letras.indexOf(caracter) < 0)
+                return false;
+        })
+        $('.txtFolio').keypress(function(e)
+        {
+            // obtener el caracter presionado por el usuario
+            // obtener el caracter presionado por el usuario
+            let caracter = String.fromCharCode(e.which);
+            if(numeros.indexOf(caracter) < 0)
+                return false;
+        })
+    
+        $('.txtValor').keypress(function(e)
+        {
+            // obtener el caracter presionado por el usuario
+            // obtener el caracter presionado por el usuario
+            let caracter = String.fromCharCode(e.which);
+            if(numeros.indexOf(caracter) < 0)
+                return false;
+        })
+    
+        $('.txtEmail').keypress(function(e)
+        {
+            let patron = letras + numeros + '@.-';
+            let caracter = String.fromCharCode(e.which);
+            if(patron.indexOf(caracter) < 0)
+                return false;
+        })
+    
+        $('.btnLimpiar').click(function()
+        {
+            $('.txtFolio, .txtValor').val('');
+            
+        });
+    
 
     
-    $('.txtApellido').keypress(function(e)
-    {
-        // obtener el caracter presionado por el usuario
-        let caracter = String.fromCharCode(e.which);
-        if(letras.indexOf(caracter) < 0)
-            return false;
-    })
-    $('.txtNombre').keypress(function(e)
-    {
-        // obtener el caracter presionado por el usuario
-        let caracter = String.fromCharCode(e.which);
-        if(letras.indexOf(caracter) < 0)
-            return false;
-    })
-  
-
-
-    $('.txtEmail').keypress(function(e)
-    {
-        let patron = letras + numeros + '@._-';
-        let caracter = String.fromCharCode(e.which);
-        if(patron.indexOf(caracter) < 0)
-            return false;
-    })
-
-    $('.btnLimpiar').click(function()
-    {
-        $('.txtNombre, .txtApellido, .txtTelefono, .txtEmail, .txtContraseña, .txtConfirmesucontraseña').val('');
-        $('.txtNombre').focus();
+        $('.btnRegistrar').click(function()
+        {
+    
+            if(!$.trim($('.txtFolio').val()))
+            {
+                alert("debe especificar el folio");
+                $('.txtFolio').focus();
+            }
+            else  if(!$.trim($('.txtValor').val()))
+            {
+                alert("Debe especificar el valor");
+                $('.').focus();
+            }
+           
+        })
+    
+    
+    
     });
-
-
-    let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/
-
-    $('.btnRegistrar').click(function()
-    {
-        
-        if(!$.trim($('.txtNombre').val()))
-        {
-            alert("Debe especificar nombre");
-            $('.txtNombre').focus();
-        }
-        else  if(!$.trim($('.txtApellido').val()))
-        {
-            alert("Debe especificar apellido");
-            $('.txtApellido').focus();
-        }
-        else  if(!$.trim($('.txtTelefono').val()))
-        {
-            alert("Debe especificar telefono");
-            $('.txtTelefono').focus();
-        }
-
-        else  if(!emailRegex.test(($('.txtEmail').val())))
-        {
-            alert("El formato del correo no es válido");
-            $('.txtEmail').focus();
-        }
-
-        else  if(!$.trim($('.txtContraseña').val()))
-        {
-            alert("El formato del correo no es válido");
-            $('.txtContraseña').focus();
-        }
-
-        else  if(!$.trim($('.txtConfirmesucontraseña').val()))
-        {
-            alert("El formato del correo no es válido");
-            $('.txtConfirmesucontraseña').focus();
-        }
-
-
-
-    })
-
-     
-
-});
